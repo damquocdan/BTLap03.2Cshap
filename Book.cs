e052cfd474184828fb978202d753b012973186ed
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2023-07-28 21:53:10 +0700

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTLap03._2Cshap
+{
+    internal class Book
+    {
+        private string author;
+        private int pages;
+        private string isbn;
+        private string title;
+        private int curentpage;
+
+        public Book()
+        {
+            author = "";
+            pages = 0;
+            isbn = "";
+            title = "";
+            curentpage = 1;
+        }
+        public Book(string author,int page,string isbn,string title,string curentpage)
+        {   
+            this.author = author;
+            this.pages = page;
+            this.isbn = isbn;
+            this.title = title;
+
+        }
+        public void Display()
+        {
+            Console.WriteLine("tác giả"+author);
+            Console.WriteLine("trang:" +pages);
+            Console.WriteLine("isbn:" + isbn);
+            Console.WriteLine("Tiêu đề:" + title);
+            Console.WriteLine("Trang hiện tại:" + curentpage);
+        }
+        public void flipPageForWard()
+        {
+            Console.WriteLine("Lật sang trang trước!");
+        }
+        public void flipPageBackward()
+        {
+            Console.WriteLine("Lật sang trang sau");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTLap03._2Cshap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Book b1= new Book();
+            Book b2 = new Book("Đàm Quốc Dân",10,"VN","Đắc nhân Tâm","50");
+            b1.Display();
+            b2.Display();
+
+            b1.flipPageForWard();   
+            b1.flipPageBackward();
+            Console.ReadKey();
+        }
+    }
+}
